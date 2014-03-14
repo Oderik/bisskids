56526e240ce73aef400b7a7dbcac76de6f3043cb
Friedger <friedger@gmail.com>
2014-03-14 15:06:59 +0100

    add talking time

--- talkinglemon.py
+++ talkinglemon.py
@@ -1,19 +1,34 @@
 from bottle import route, run, debug, template, request
-
+import pyttsx
+import thread
+import time
 
 @route('/say', method='GET')
 def say():
     text = request.GET.get('text', '')
+    thread.start_new_thread( say, (text, ) )    	
     return text
 
 @route('/count', method='GET')
 def count():
     limit = request.GET.get('limit', 10)
 
+    engine.say("Go now!")
+    engine.runAndWait()
+
     result = ""
     for number in range(1, int(limit) + 1):
         result += str(number) + ", "
-
+	thread.start_new_thread( say, (number, ) )  
+	time.sleep(1)
     return result
 
-run(port=80)
+def say(text):
+    if engine.isBusy():
+	engine.stop()
+
+    engine.say(text)
+    engine.runAndWait()
+	
+engine = pyttsx.init()
+run(port=8080)
